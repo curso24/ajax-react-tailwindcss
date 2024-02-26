153b6d22b11363be861946e7f5361ffd947c5b76
juanda <juandacorreo@gmail.com>
2024-02-26 10:54:09 +0100

    ficheros

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Tabla from './Tabla'
 function App() {
+  // hacer llamada API
+  // para evitar que se renderice varias veces, se hace en un useEffect
+
+  // inserta una llamada api
+  const [categorias, setCategorias] = React.useState([])
+  const [platos, setPlatos] = React.useState([])
+  const [restaurantes, setRestaurantes] = React.useState([])
+
+  React.useEffect(() => {
+    fetch('https://platzi-avo.vercel.app/api/categorias')
+      .then((res) => res.json())
+      .then((categorias) => setCategorias(categorias))
+
+    fetch('https://platzi-avo.vercel.app/api/platos')
+      .then((res) => res.json())
+      .then((platos) => setPlatos(platos))
+
+    fetch('https://platzi-avo.vercel.app/api/restaurantes')
+      .then((res) => res.json())
+      .then((restaurantes) => setRestaurantes(restaurantes))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Lista de Restaurantes</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Tabla
+        restaurantes={restaurantes}
+        platos={platos}
+        categorias={categorias}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
