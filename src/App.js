366d256681c88d530888dc4fed0143326e4ddd2e
juanda <juandacorreo@gmail.com>
2024-02-26 11:30:59 +0100

    ajax call ok

--- src/App.js
+++ src/App.js
@@ -1,36 +1,46 @@
+import { useState, useEffect } from 'react'
 import Tabla from './Tabla'
+import API from './api'
 function App() {
   // hacer llamada API
   // para evitar que se renderice varias veces, se hace en un useEffect
 
   // inserta una llamada api
-  const [categorias, setCategorias] = React.useState([])
-  const [platos, setPlatos] = React.useState([])
-  const [restaurantes, setRestaurantes] = React.useState([])
+  const [categorias, setCategorias] = useState([])
+  const [platos, setPlatos] = useState([])
+  const [restaurantes, setRestaurantes] = useState([])
 
-  React.useEffect(() => {
-    fetch('https://platzi-avo.vercel.app/api/categorias')
+  // useEffect (function, [dependencies]). Si no hay dependencias
+  // se ejecuta una sola vez
+
+  useEffect(() => {
+    fetch(API.CATEGORIES_URL)
       .then((res) => res.json())
       .then((categorias) => setCategorias(categorias))
-
-    fetch('https://platzi-avo.vercel.app/api/platos')
+    fetch(API.DISHES_URL)
       .then((res) => res.json())
       .then((platos) => setPlatos(platos))
-
-    fetch('https://platzi-avo.vercel.app/api/restaurantes')
+    fetch(API.RESTAURANTS_URL)
       .then((res) => res.json())
       .then((restaurantes) => setRestaurantes(restaurantes))
-  }, [])
+  }, []) //El corchete vacío es para una única llamada a la API
+
+  const datosCargados = Boolean(
+    restaurantes.length && platos.length && categorias.length
+  )
 
   return (
     <div>
       <h1>Lista de Restaurantes</h1>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Tabla
-        restaurantes={restaurantes}
-        platos={platos}
-        categorias={categorias}
-      />
+      {datosCargados && (
+        <Tabla
+          restaurantes={restaurantes}
+          platos={platos}
+          categorias={categorias}
+        />
+      )}
+      {!datosCargados && <p>Cargando datos...</p>}
     </div>
   )
 }
---
+++ src/api.js
@@ -0,0 +1,12 @@
+const SERVER_URL = 'https://juanda.certweb.infenlaces.com'
+const CATEGORIES_URL = `${SERVER_URL}/categories`
+const DISHES_URL = `${SERVER_URL}/dishes`
+const RESTAURANTS_URL = `${SERVER_URL}/restaurants`
+
+const API = {
+  CATEGORIES_URL,
+  DISHES_URL,
+  RESTAURANTS_URL,
+}
+
+export default API
